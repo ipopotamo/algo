41bc2dd0d28c836deb19dcfe3b8e48de0d1edf43
ipopotamo <ignaciobarbieri2003@gmail.com>
2022-10-24 19:12:59 -0300

    a dios le pido que si me muero sea de amor y si me enamoro sea de vos y que tu voz sea del corazon todos los dias a dios le pido

--- nose/untitled/src/test/calcuadora.java
+++ nose/untitled/src/test/calcuadora.java
@@ -9,30 +9,57 @@ public class calcuadora {
 
     calcu cals = new calcu();
 
+//sale bien
     @Test
-    public void sumarTest(){
-        assertNotEquals(cals.sumar(5,3),5);
+    public void sumarTest() {
+        assertNotEquals(cals.sumar(5, 3), 5);
     }
 
     @Test
-    public void restarTest(){
-        assertEquals(cals.resstar(1,1),0);
+    public void restarTest() {
+        assertEquals(cals.resstar(1, 1), 0);
     }
+
     @Test
-    public void divTest(){
-        assertEquals(cals.div(3,3),1);
+    public void divTest() {
+        assertEquals(cals.div(3, 3), 1);
     }
+
     @Test
-    public void porTest(){
-        assertEquals(cals.por(2,3),6);
+    public void porTest() {
+        assertEquals(cals.por(2, 3), 6);
     }
+
     @Test
-    public void cuaTest(){
-        assertEquals(String.valueOf(new float[][]{cals.juan(1, 2, 1)}),1,1);
+    public void cuaTest() {
+        assertEquals(String.valueOf(new float[][]{cals.juan(1, 2, 1)}), 1, 1);
     }
 
+//sale mal
 
+    @Test
+    public void sumarTestM() {
+        assertNotEquals(cals.sumar(5, 2), 5);
+    }
 
+    @Test
+    public void restarTestM() {
+        assertEquals(cals.resstar(1, 6), 0);
+    }
 
+    @Test
+    public void divTestM() {
+        assertEquals(cals.div(444, 3), 1);
+    }
+
+    @Test
+    public void porTestM() {
+        assertEquals(cals.por(1, 3), 6);
+    }
+
+    @Test
+    public void cuaTestM() {
+        assertEquals(String.valueOf(new float[][]{cals.juan(1, 2, 1)}), 55, 4);
+
+    }
 }
-
